3eb34c7c659b0f000f55eadd7a74630411d513cb
mp4402 <mariopisquiy@ufm.edu>
2020-10-14 15:59:51 -0600

    Create Producto.java
    Se creo el primero objeto, de tipo Producto

---
+++ Producto.java
@@ -0,0 +1,23 @@
+public class Producto{
+	String codigo;
+	double precio;
+	String marca;
+	String nombre;
+	String modelo;
+	String tipoMaterial;
+	int peso;
+	int cantidad;
+	int noBodega;
+	int activo;
+	public Producto(String c, double p, String ma, String n, String mo, String tm, int pe, int can, int noB){
+		this.codigo = c;
+		this.precio = p;
+		this.marca = ma;
+		this.nombre = n;
+		this.modelo = mo;
+		this.peso = pe;
+		this.cantidad = can;
+		this.noBodega = noB;
+		this.activo = 1;
+	}
+}
